391badbfed9622a21215b13fbd2559154c1f8738
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-06-07 18:29:41 +0700

    preset for JSX

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-class App {
+// class App {
+//
+//   run = async (name = 'World') => {
+//     console.log(`Hello ${name}`);
+//     console.log([1, 2, [3, 4]].flat());
+//   }
+//
+// }
+//
+// const app = new App();
+// app.run()
+//   .then(() => console.log('done'))
+//   .catch(() => console.log('Catch!!!'));
 
-  run = async (name = 'World') => {
-    console.log(`Hello ${name}`);
-    console.log([1, 2, [3, 4]].flat());
-  }
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-}
+const App = () => <p>Hello!</p>;
 
-const app = new App();
-app.run()
-  .then(() => console.log('done'))
-  .catch(() => console.log('Catch!!!'));
+ReactDOM.render(<App />, document.getElementById('root'));
